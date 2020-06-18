313b7cc50c2330a67d82d821b2b9d6c5056725a2
rapric2115 <raphael.richardson@gmail.com>
2020-06-17 22:07:53 -0400

    updating files
    updating files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+
+// components
 import Navigation from './components/navbar';
 import Home from './components/home';
+import Register from './components/register';
+import TransferForm from './components/transferForm'
 
+
+import firebase from './components/firebase';
+import {Router} from '@reach/router';
 import 'react-bootstrap'
 
 class App extends Component {
@@ -13,14 +20,29 @@ class App extends Component {
         user: null
       };
     }
+
+    componentDidMount() {
+      const ref = firebase.database().ref('user');
+
+      ref.on('value', snapshot => {
+        let FUser = snapshot.val();
+        console.log(FUser);
+        this.setState({user: FUser});
+      })
+    }
  
   
     render () {
         return (
           <div className="App">
             <Navigation user={this.state.user}/>
+
             <header className="App-header">
-              <Home />
+              <Router>
+              <Home path="/" user={this.state.user}/>
+              <Register path="/register"/>
+              <TransferForm path="/transferForm" />
+              </Router>
             </header>
           </div>
         );
