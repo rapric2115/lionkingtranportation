4a72fd5fac7eb8180ab9474106a2624c4db950f7
rapric2115 <raphael.richardson@gmail.com>
2020-10-13 23:47:30 -0400

    uploading all changes
    all updates made it in the page.

---
+++ precache-manifest.6c20fabb32959ef08c0d9d126b0aa4ab.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0fa95501d14b27da4ac9eed6bc5dd19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4116b227f0006ff9415",
+    "url": "/static/css/2.9c2f8ba7.chunk.css"
+  },
+  {
+    "revision": "66ec8f1bd76ebc5263f2",
+    "url": "/static/css/main.14d56bcb.chunk.css"
+  },
+  {
+    "revision": "e4116b227f0006ff9415",
+    "url": "/static/js/2.6d5e31d0.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/static/js/2.6d5e31d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ec8f1bd76ebc5263f2",
+    "url": "/static/js/main.0db480a4.chunk.js"
+  },
+  {
+    "revision": "cce0b0efd32f7b9f6c62",
+    "url": "/static/js/runtime-main.65dca608.js"
+  },
+  {
+    "revision": "923823c9cbe68ddad5ed4f7ee61d975f",
+    "url": "/static/media/2016ChryslerTownCountry.923823c9.png"
+  },
+  {
+    "revision": "4481a7bd53565af979c5978ce4e916db",
+    "url": "/static/media/Honda-Accord-Sport-1.5T-2020.4481a7bd.jpg"
+  },
+  {
+    "revision": "bf5505b083eaef74c73101008eb8538e",
+    "url": "/static/media/Hyundai-Universe.bf5505b0.jpg"
+  },
+  {
+    "revision": "913bc57c7bb24345c2baf0ba25e68b99",
+    "url": "/static/media/boat(1).913bc57c.jpg"
+  },
+  {
+    "revision": "4e7b468ee4b44757c1a3a3144d000432",
+    "url": "/static/media/boat(2).4e7b468e.jpg"
+  },
+  {
+    "revision": "ab82a7fa32558d6fc01e880953e1cd8e",
+    "url": "/static/media/chevrolet-sub17.ab82a7fa.jpg"
+  },
+  {
+    "revision": "50984425e2e6ae1e870d9c794431f753",
+    "url": "/static/media/hyundai-h12020.50984425.jpg"
+  },
+  {
+    "revision": "72d4c54a6da6f1bb1691a0dc21a9904b",
+    "url": "/static/media/hyundai-sonata-y20-2017.72d4c54a.jpg"
+  },
+  {
+    "revision": "10ae7243cdec686c25d39137089aa95e",
+    "url": "/static/media/jetski.10ae7243.jpg"
+  },
+  {
+    "revision": "df1b27edfefd2effe246b7a3c6ebbf4f",
+    "url": "/static/media/toyo-hiace-20.df1b27ed.png"
+  },
+  {
+    "revision": "9d30d1bf2932cbbd998e26759e054e2c",
+    "url": "/static/media/toyota-highlander20.9d30d1bf.png"
+  },
+  {
+    "revision": "99d86fa6d3b6dba0a7d2f05b6ba36d9d",
+    "url": "/static/media/toyota-sienna-limited-2018.99d86fa6.jpg"
+  },
+  {
+    "revision": "1c5cfaf56c4cb52afea1a52b43981632",
+    "url": "/static/media/undraw_scooter_aia8.1c5cfaf5.svg"
+  }
+]);
